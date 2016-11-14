a0b1c46dc02f09aa84467a29952cf47caba41701
Dmitry.Egorov <Dmitry.Egorov@NB-O-NZXB2>
2016-11-14 17:35:04 +0300

    1

--- wpf_EMUL_TEST/MainWindow.xaml.cs
+++ wpf_EMUL_TEST/MainWindow.xaml.cs
@@ -21,5 +21,88 @@ namespace wpf_EMUL_TEST {
         public MainWindow() {
             InitializeComponent();
         }
+
+
+        #region CnvEMUL_Draw
+        
+        Point currentPoint = new Point();
+        
+        private void CnvEmul_MouseMove(object sender, MouseEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                Line line = new Line();
+
+                line.Stroke = SystemColors.WindowFrameBrush;
+                line.X1 = currentPoint.X;
+                line.Y1 = currentPoint.Y;
+                line.X2 = e.GetPosition((Canvas)sender).X;
+                line.Y2 = e.GetPosition((Canvas)sender).Y;
+
+                currentPoint = e.GetPosition((Canvas)sender);
+
+
+                DateTime d = DateTime.Now;
+                if (d.Millisecond % 1 == 0){
+                    CnvEmul.Children.Add(line);
+                }
+                
+            }
+        }
+        private void CnvEmul_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition((Canvas)sender);
+        }
+
+        private void CnvEmul_MouseRightButtonDown(object sender, MouseButtonEventArgs e) {
+            CnvEmul.Children.Clear();
+        }
+
+        /*
+        Line newLine;
+        Point start;
+        Point end;
+
+        private void CnvEmul_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed){
+                start = e.GetPosition((Canvas) sender);
+            }
+        }
+        private void CnvEmul_MouseMove(object sender, MouseEventArgs e) {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                end = e.GetPosition((Canvas)sender);
+            }
+        }
+       
+
+        private void CnvEmul_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            newLine = new Line();
+            newLine.Stroke = SystemColors.WindowFrameBrush;
+            newLine.X1 = start.X;
+            newLine.Y1 = start.Y;
+            newLine.X2 = end.X;
+            newLine.Y2 = end.Y;
+
+            CnvEmul.Children.Add(newLine);
+        }
+        */
+        #endregion
+        private void bt_cnv_draw_Click(object sender, RoutedEventArgs e){
+            Cnv.Children.Clear();
+            foreach (Line item in CnvEmul.Children){
+                Rectangle rec = new Rectangle(){
+                    Height = 2,
+                    Width = 2,
+                    Fill = Brushes.Red
+                };
+
+                Canvas.SetTop(rec, item.Y1);
+                Canvas.SetLeft(rec, item.X1);
+                /*
+                Canvas.SetTop(rec, Canvas.GetTop(item));
+                Canvas.SetLeft(rec, Canvas.GetLeft(item));*/
+                //MessageBox.Show($"{Canvas.GetTop(item)}");
+                Cnv.Children.Add(rec);
+            }
+        }
+        
     }
 }
